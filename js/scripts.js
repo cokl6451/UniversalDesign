dc1db21c056ad8c2eaa02ccd4b712aeddf5f23c2
cokl6451 <95327102+cokl6451@users.noreply.github.com>
2023-11-06 10:08:07 -0700

    add grid

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
+// Create local storage for fish names and opinions
+let names = [];
+let opinions = [];
+localStorage.setItem('names', names);
+localStorage.setItem('opinions', opinions);
 
 document.getElementById('fishForm').addEventListener('submit', function(e) {
     e.preventDefault();
@@ -9,7 +14,26 @@ document.getElementById('fishForm').addEventListener('submit', function(e) {
     var opinionCell = newRow.insertCell();
     fishCell.textContent = name;
     opinionCell.textContent = opinion;
+
+    // Testing local data storage
+    localStorage.setItem('name', name);
+    localStorage.setItem('opinion', opinion);
+    console.log(localStorage.getItem('name'));
+    console.log(localStorage.getItem('opinion'));
+    // Create an array of all that has been added to the table so we can fetch
+    let names = [];
+    let opinions = [];
+    names.push(localStorage.getItem('name'));
+    opinions.push(localStorage.getItem('opinion'));
+
     document.getElementById('name').value = '';
     document.getElementById('opinion').value = '';
 });
+
+document.getElementById('fishTable').addEventListener('load', function(e) {
+    e.preventDefault();
+    console.log("Table loaded");
+    console.log(localStorage.getItem('names'));
+    console.log(opinions);
+})
 
